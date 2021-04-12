569a9a0e4f5a84a280f4762294855152f9a52449
CFTM78 <67889716+CFTM78@users.noreply.github.com>
2021-04-12 22:12:06 +0800

    lasso

--- 统计学习方法/大报告/code/lasso1.R
+++ 统计学习方法/大报告/code/lasso1.R
@@ -1,9 +1,11 @@
+set.seed(123)
+library(glmnet)
 library(msgps)
 library(lars)
 library(MASS)
 X <- model.matrix(medv~.,Boston)[,-1]
 y <- Boston$medv
-n <- dim(X)[1];p <- dim(X)[2]
+n <- dim(X)[1];p <- (dim(X)[2]+1)
 
 
 y1 = rep(0,n)
@@ -16,13 +18,14 @@ y1 = rep(0,n)
 # X=t(x)
 # n=ncol(X)
 # p=10
-X = t(X)
+con <- rep(1,n)
+X1 = rbind(con,t(X))
 
 #给出初始wi，转换X数据
 
 ##对转换后的数据做LASSO
 ##设超参数lambda
-lambda=0.01
+lambda=0.02118502
 ##采用循环坐标下降法得到估计
 cd<-function(){
   #随机给出初值betahat,以及用于迭代的暂时储存估计beta的变量
@@ -33,9 +36,9 @@ cd<-function(){
   while (sum(abs(temp1-temp2))>1e-12) {
     temp1= beta_hat
     for(i in 1:p){
-      y1=as.vector(y-beta_hat[-i]%*%X[-i,])
-      X[i,]=matrix(c(X[i,]),nrow=1,ncol=n)
-      beta_i_ols=solve(sum(X[i,]^2))%*%X[i,]%*%y1
+      y1=as.vector(y-beta_hat[-i]%*%X1[-i,])
+      X1[i,]=matrix(c(X1[i,]),nrow=1,ncol=n)
+      beta_i_ols=solve(sum(X1[i,]^2))%*%X1[i,]%*%y1
       if(abs(beta_i_ols)>lambda){
         beta_hat[i]=sign(beta_i_ols)*(abs(beta_i_ols)-lambda)
       }
@@ -52,9 +55,20 @@ beta_hat_1=cd()
 
 
 ##R自带函数结果
-
-lar1 <- lars(t(X),y,type = "lasso")
 beta_hat_1
-beta_hat_2 <- lar1$beta
-summary(lar1)
-beta_hat_2
+res<-cv.glmnet(x=X,y=y,family="gaussian",alpha = 1,nfolds = 10)
+# setEPS()
+# postscript("lassocv.eps")
+plot(res)
+# dev.off()
+# lar1 <- lars(t(X),y,type = "lasso")
+# beta_hat_1
+# beta_hat_2 <- lar1$beta
+# summary(lar1)
+# beta_hat_2
+rmin <- glmnet(x=X,y=y,family = "gaussian",alpha = 1,lambda = res$lambda.min)
+coef(rmin)
+rmin.fit <-  t(t(coef(rmin))%*%X1)
+rmin.mse <-  mean((rmin.fit-y)^2)
+ofit <- t(t(beta_hat_1)%*%X1)
+omse <- mean((ofit-y)^2)
